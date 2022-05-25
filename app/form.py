8856b780d61168e0c2d86f0e92fc4be82c07d035
ismatillo-ismatov <ismatilloismatov1995@gmail.com>
2022-05-25 09:42:35 +0500

    'c'

---
+++ app/form.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.forms import UserCreationForm,UsernameField
+from django.contrib.auth import get_user_model
+from django import forms
+from .models import Lead
+
+User = get_user_model()
+
+class LeadModelForm(forms.ModelForm):
+    class Meta:
+        model = Lead
+        fields = (
+            "first_name",
+            "last_name",
+            "age",
+            "agent",
+        )
+
+
+class LeadForm(forms.Form):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    age = forms.IntegerField(min_value=0)
+
+class CustomCreateForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username",)
+        fields_classes = {"username":UsernameField}
